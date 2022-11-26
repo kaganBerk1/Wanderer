ca2609eeeed00b798c59cec667363a4ed7d8496a
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-11-27 00:30:05 +0300

    Auth handled

---
+++ Api/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+export default axios.create({
+    baseURL:"https://25d4-31-223-87-41.eu.ngrok.io",
+
+})
+
--- pages/Login.js
+++ pages/Login.js
@@ -1,36 +1,106 @@
 
-import { useState } from "react";
-import { View,Text ,Button,StyleSheet, Dimensions} from "react-native";
-import LoginScreen, { SocialButton } from "react-native-login-screen";
+import { useEffect, useState } from "react";
+import { View,Text ,Button,StyleSheet, Dimensions, Keyboard,SafeAreaView,Image,TouchableOpacity,ActivityIndicator,} from "react-native";
 import { globalStyles } from "../styles/global";
 import Modal from "react-native-modal";
 import { TextInput } from "react-native-gesture-handler";
 import { AntDesign } from '@expo/vector-icons';
+import axios from "axios";
+import {useNavigation,useIsFocused} from "@react-navigation/native"
+
+const ScreenWidth= Dimensions.get("screen").width;
 
 export default function Login() {
-    const [isModalVisible, setModalVisible] = useState(false);
+    const [isModalVisible, setModalVisible] = useState(false)
+    const [loading, setLoading] = useState(false);
     const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState(0);
+    const [password, setPassword] = useState("");
     const [newUserName, setNewUserName] = useState("");
     const [newPassword, setNewPassword] = useState(0);
-    const [newPasswordConfirm, setPasswordConfirm] = useState(0);
-
-    
+    const [newPasswordConfirm, setNewPasswordConfirm] = useState(0);
+    const navigation=useNavigation();
+    const isFocused = useIsFocused();
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
 
+  useEffect(() => {
+    setModalVisible(false);
+    setUserName("")
+    setNewUserName("")
+    setPassword("")
+    setNewPassword("")
+    setNewPasswordConfirm("")
+}, [isFocused]);
+
   function handleLogin(){
-    //TODO: Login and log out function should created When backend ready
-    alert("log in or out")
-  }
+    setLoading(true);
+    axios({
+        method: 'post',
+        url: 'https://25d4-31-223-87-41.eu.ngrok.io/auth/login',
+        data: {
+          name: userName,
+          password: password
+        }
+    }).then(res => {
+        setLoading(false)
+        navigation.navigate("Home")
+
+    }).catch((err)=>{
+        //console.log(err)
+      alert("Invalid User Name or Password")
+      setLoading(false)
+    })
+
+
+}
+
+  
 
   function handleNewUser(){
-    //TODO: create new user When backend ready
-    //TODO: Check Username is unique in database
+    setLoading(true)
+    if(newPassword===newPasswordConfirm){
+        axios({
+            method: 'post',
+            url: 'https://25d4-31-223-87-41.eu.ngrok.io/auth/register',
+            data: {
+              name: newUserName,
+              password: newPassword
+            }
+        }).then(res => {
+            setLoading(false)
+            navigation.navigate("Home")
+        }).catch((err)=>{
+            console.log(err)
+            alert("Uour User Name is not unique")
+            setLoading(false)
 
-    alert("new user")
+        })
+    }else{
+        alert("passwords do not match")
+    }
   }
+
+  const Divider = () => <View style={[styles.dividerStyle]} />;
+
+  const LoginButton = ({type}) => (
+    <TouchableOpacity
+      style={type=="login"?styles.loginButtonStyle:styles.createButton}
+      onPress={type==="login"?handleLogin:handleNewUser}>
+      <Text style={[styles.loginTextStyle]}>
+        {type==="login"?"Login":"create"}
+      </Text>
+    </TouchableOpacity>
+  );
+
+
+const Logo = () => (
+    <Image
+        resizeMode="contain"
+        source={require("../assets/logo.png")}
+        style={[styles.logoImageStyle]}
+    />
+);
 
   return (
         <View style={[globalStyles.container,{marginTop:30}]}>
@@ -41,57 +111,60 @@ export default function Login() {
                     fontFamily:"nunito-bold"
                 }}>Wanderer</Text>
             </View>
-            <LoginScreen
-            logoImageSource={require("../assets/logo.png")}
-            onLoginPress={() => {handleLogin()}}
-            onSignupPress={toggleModal}
-            onEmailChange={(value) => {setUserName(value)}}
-            onPasswordChange={(value) => {setPassword(value)}}
-            disableSocialButtons
-            emailPlaceholder="Username"
-            style={{
-                backgroundColor:"transparent"
-            }}
-            >
-            </LoginScreen>
+            <SafeAreaView style={[styles.container, styles]}>
+                <Logo />
+                <View  style={
+                    {
+  
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#acabb0",
+                    }
+                }>
+                  <TextInput value={userName} onChangeText={(value)=>setUserName(value)}  style={[styles.comment,{marginBottom:20}]}  multiline placeholder='Username'></TextInput>
+                  <TextInput  value={password}   onChangeText={(value)=>setPassword(value)}   style={styles.title}  secureTextEntry={true}   placeholder="Password"></TextInput>
+                </View>
+                {
+                  loading?
+                  <ActivityIndicator style={{marginTop:20}} color="#acabb0" size="large" />
+                  :
+                  <LoginButton type="login"/>
+                }
+                <TouchableOpacity
+                style={
+                    {
+                        marginTop: 32,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#acabb0",
+                    }
+                }
+                    onPress={toggleModal}>
+                    <Text style={{
+                      color: "#acabb0",
+                    }}>
+                        {"Create a account"}
+                    </Text>
+                </TouchableOpacity>
+                <Divider />
+            </SafeAreaView>
 
             <Modal   isVisible={isModalVisible}   onBackdropPress={() => setModalVisible(false)} >
                 <View style={{ height: Dimensions.get('window').height*0.4,backgroundColor:"white",borderRadius:30, }}>
                 <AntDesign onPress={()=>setModalVisible(false)} name="closecircleo" style={styles.close} size={36} color="black" />
                     <View style={styles.textCover}>
-                        <TextInput  onChangeText={(value)=>setNewUserName(value)}  style={styles.comment}  multiline placeholder='Username'></TextInput>
-                        <TextInput   onChangeText={(value)=>setNewPassword(value)}   style={styles.title}  secureTextEntry={true}  keyboardType='numeric' placeholder="Password"></TextInput>
-                        <TextInput  onChangeText={(value)=>setPasswordConfirm(value)}  style={styles.title}  secureTextEntry={true}  keyboardType='numeric' placeholder="Password Again"></TextInput>
+                        <TextInput value={newUserName} onChangeText={(value)=>setNewUserName(value)}  style={styles.comment}  multiline placeholder='Username'></TextInput>
+                        <TextInput  value={newPassword}  onChangeText={(value)=>setNewPassword(value)}   style={styles.title}  secureTextEntry={true}   placeholder="Password"></TextInput>
+                        <TextInput value={newPasswordConfirm}  onChangeText={(value)=>setNewPasswordConfirm(value)}  style={styles.title}  secureTextEntry={true}   placeholder="Password Again"></TextInput>
 
                     </View>
 
-                    <LoginScreen
-                        logoImageSource={require("../assets/logo.png")}
-                        onLoginPress={() => {handleNewUser()}}
-                        disableSignup
-                        loginButtonText="Create"
-                        disableSocialButtons
-                        disableDivider
-                        logoImageStyle={{
-                            display:"none"
-                        }}
-                        style={{
-                            backgroundColor:"transparent",
-                            margin:0,
-                            padding:0,
-                        }}
-                        loginButtonStyle={{
-                            width:"90%",
-                            margin:0,
-                            padding:0,
-                        }}
-                        textInputContainerStyle={
-                            {
-                            display:"none"         
-                            }
-                        }
-                        >
-                        </LoginScreen>
+                    {
+                  loading?
+                  <ActivityIndicator style={{marginTop:20}} color="#acabb0" size="large" />
+                  :
+                  <LoginButton/>
+                }
 
 
               {/*       <Button title="Hide modal" onPress={toggleModal} /> */}
@@ -130,7 +203,9 @@ const styles= StyleSheet.create({
         paddingHorizontal:10,
         paddingVertical:12,
         fontFamily:"nunito-bold",
-        fontSize:18
+        fontSize:18,
+        alignItems:"center",
+       
   
     },
     textCover:{
@@ -143,5 +218,87 @@ const styles= StyleSheet.create({
         right:8,
         zIndex:20,
         top:8,
-    }
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#f7f7f7",
+      },
+      logoImageStyle: {
+        width: 200,
+        height: 200,
+        alignSelf: "center",
+      },
+      textInputContainer: {
+        marginTop: 12,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      passwordTextInputContainer: {
+        marginTop: 16,
+      },
+      loginButtonStyle: {
+        height: 40,
+        width: ScreenWidth * 0.9,
+        backgroundColor: "#25a9e2",
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center",
+        alignSelf: "center",
+        marginTop: 32,
+        elevation: 5,
+        shadowRadius: 8,
+        shadowOpacity: 0.3,
+        shadowColor: "#166080",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+      },
+      createButton:{
+        height: 40,
+        width: ScreenWidth * 0.7,
+        backgroundColor: "#25a9e2",
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center",
+        alignSelf: "center",
+        marginTop: 32,
+        elevation: 5,
+        shadowRadius: 8,
+        shadowOpacity: 0.3,
+        shadowColor: "#166080",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+      },
+      loginTextStyle: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+      },
+      signupStyle: {
+        marginTop: 32,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      signupTextStyle: {
+        color: "#acabb0",
+      },
+      dividerStyle: {
+        height: 0.5,
+        marginTop: 24,
+        marginBottom: 12,
+        borderRadius: 16,
+        width: ScreenWidth * 0.8,
+        alignSelf: "center",
+        backgroundColor: "#ccc",
+      },
+      socialLoginContainer: {
+        marginTop: 16,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    
+    
   })
